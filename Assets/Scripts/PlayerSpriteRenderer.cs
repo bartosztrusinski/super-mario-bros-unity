9f0f5b9da55234951f52a301b114fbb8ec1d406b
Bartosz Trusiński <bartosztrusinski@gmail.com>
2024-06-28 00:42:12 +0200

    animate player run

--- Assets/Scripts/PlayerSpriteRenderer.cs
+++ Assets/Scripts/PlayerSpriteRenderer.cs
@@ -3,9 +3,9 @@ using UnityEngine;
 public class PlayerSpriteRenderer : MonoBehaviour
 {
     public Sprite idle;
-    public Sprite run;
     public Sprite jump;
     public Sprite slide;
+    public AnimatedSprite run;
 
     private SpriteRenderer spriteRenderer;
     private PlayerMovement playerMovement;
@@ -16,12 +16,23 @@ public class PlayerSpriteRenderer : MonoBehaviour
         playerMovement = GetComponentInParent<PlayerMovement>();
     }
 
+    void OnEnable()
+    {
+        spriteRenderer.enabled = true;
+    }
+
+    void OnDisable()
+    {
+        spriteRenderer.enabled = false;
+    }
+
     void LateUpdate()
     {
+        run.enabled = playerMovement.isRunning;
+
         spriteRenderer.sprite =
             playerMovement.isJumping ? jump
             : playerMovement.isTurning ? slide
-            : playerMovement.isRunning ? run
-            : idle;
+            : !playerMovement.isRunning ? idle : spriteRenderer.sprite;
     }
 }
