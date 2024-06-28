ee8d0322ee202827f831641b024d1768e24f3f8d
Bartosz Trusiński <bartosztrusinski@gmail.com>
2024-06-28 15:26:53 +0200

    add goomba stomp

---
+++ Assets/Scripts/Goomba.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class Goomba : MonoBehaviour
+{
+    public Sprite flattenedSprite;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (IsCollidingWithPlayer(collision) && IsGettingStomped(collision))
+        {
+            Flatten();
+        }
+    }
+
+    private bool IsCollidingWithPlayer(Collision2D collision)
+    {
+        return collision.gameObject.CompareTag("Player");
+    }
+
+    private bool IsGettingStomped(Collision2D collision)
+    {
+        return collision.transform.DotProduct(transform, Vector2.down) > 0.3f;
+    }
+
+    private void Flatten()
+    {
+        GetComponent<Collider2D>().enabled = false;
+        GetComponent<EntityMovement>().enabled = false;
+        GetComponent<AnimatedSprite>().enabled = false;
+        GetComponent<SpriteRenderer>().sprite = flattenedSprite;
+        Destroy(gameObject, 1f);
+    }
+}
